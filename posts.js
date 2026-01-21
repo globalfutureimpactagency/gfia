c047b85929165b666b19ba1dff29514bae3e7eec
globalfutureimpactagency <nishathkhan.f@gmail.com>
2026-01-21 11:28:21 +0400

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,9 @@
+[
+  {
+    "title": "Sample Post Title",
+    "subtitle": "Short subheading for the article",
+    "date": "2026-01-21 10:30",
+    "image": "news1.jpg",
+    "url": "news/article-1.html"
+  }
+]
